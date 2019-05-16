0a83bd068ec164587ba2b4e31895849f11110785
Corintio <corintio@cloud>
2019-05-16 08:06:42 -0400

    Add user-agent to backup's rclone invocation

--- defaults/borgmatic/upload_backup.sh
+++ defaults/borgmatic/upload_backup.sh
@@ -9,4 +9,5 @@ docker exec rclone rclone sync -v \
     --config /config/.rclone.conf \
     --transfers=10 \
     --checkers=20 \
+    --user-agent "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1" \
     /data/backups gcrypt:/backups
