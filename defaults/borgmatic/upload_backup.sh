b520eaa80c4747791d1468a03a6aa99878241a92
Corintio <corintio@cloud>
2019-05-16 22:39:57 -0400

    Reduced the required API calls for uploads

--- defaults/borgmatic/upload_backup.sh
+++ defaults/borgmatic/upload_backup.sh
@@ -6,7 +6,7 @@ which docker && apk add --no-cache docker
 # Execute the command rclone sync in the rclone containter
 docker exec rclone rclone sync -v \
     --checksum \
-    --transfers=10 \
-    --checkers=20 \
-    --user-agent "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1" \
+    --transfers 3 \
+    --checkers 3 \
+    --tpslimit 3 \
     /data/backups gcrypt:/backups
