eaa71f9089b2de9a410f9e051b04fec497595fe1
Corintio <corintio@cloud>
2019-05-16 23:15:52 -0400

    Fixing rclone config location :(

--- defaults/borgmatic/upload_backup.sh
+++ defaults/borgmatic/upload_backup.sh
@@ -5,6 +5,7 @@ which docker && apk add --no-cache docker
 
 # Execute the command rclone sync in the rclone containter
 docker exec rclone rclone sync -v \
+    --config /rclone/rclone.conf \
     --checksum \
     --transfers 3 \
     --checkers 3 \
